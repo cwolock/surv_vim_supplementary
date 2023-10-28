d0cb0d66c4f940cb4ebbddb1eb993cc5e9a64c63
cwolock <cwolock@uw.edu>
2023-10-28 13:01:38 -0400

    figure updates

--- sims/scenario2/figures.R
+++ sims/scenario2/figures.R
@@ -7,24 +7,24 @@ truth_list <- compile_truth(true_param_file = truth_file,
                             true_avar_file = var_truth_file)
 
 # LANDMARK SIMS
-landmark_dat <- readRDS("C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/scenario1_landmark.rds")
-landmark_summ <- summarize_results(landmark_dat, scenario = "1", truth_list$truth, truth_list$var_truth)
+landmark_dat <- readRDS("C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/scenario2_landmark.rds")
+landmark_summ <- summarize_results(landmark_dat, scenario = "2", truth_list$truth, truth_list$var_truth)
 
 # C-INDEX SIMS
-cindex_dat <- readRDS("C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/scenario1_cindex.rds")
-cindex_summ <- summarize_results(cindex_dat, scenario = "1", truth_list$truth, truth_list$var_truth)
+cindex_dat <- readRDS("C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/scenario2_cindex.rds")
+cindex_summ <- summarize_results(cindex_dat, scenario = "2", truth_list$truth, truth_list$var_truth)
 
 
 summ <- bind_rows(landmark_summ, cindex_summ)
 make_sim_plot(summ,
-              scenario = "1",
+              scenario = "2",
               big = TRUE,
               wd = "C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/",
-              fname = "scenario1_big")
+              fname = "scenario2_big")
 
-summ <- summ %>% filter(vim == "AUC" & indx == 1)
+summ <- summ %>% filter(vim == "AUC" & indx == 4)
 make_sim_plot(summ,
-              scenario = "1",
+              scenario = "2",
               big = FALSE,
               wd = "C:/Users/cwolo/Dropbox/UW/DISSERTATION/surv_vim_supplementary/scratch/",
-              fname = "scenario1_small")
+              fname = "scenario2_small")
