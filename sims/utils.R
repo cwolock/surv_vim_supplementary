75fa7e8fbe971aab60a84996bc4bba4453510477
Charlie Wolock <cwolock@gmail.com>
2024-06-18 09:54:27 -0400

    back to old SL library except glm includes interactions

--- sims/utils.R
+++ sims/utils.R
@@ -202,8 +202,8 @@ generate_full_predictions <- function(time,
     }
   } else if (nuisance == "stackG"){
     # tune <- list(ntrees = c(250, 500, 1000),
-    tune <- list(ntrees = c(1000),
-                 max_depth = c(1, 2, 3),
+    tune <- list(ntrees = c(250, 500, 1000),
+                 max_depth = c(1, 2),
                  minobspernode = 10,
                  shrinkage = 0.01)
     xgb_grid <- create.SL.xgboost(tune = tune)
@@ -250,8 +250,8 @@ generate_DR_predictions <- function(time,
                                     S_hat,
                                     G_hat){
   
-  tune <- list(ntrees = c(1000),
-               max_depth = c(1, 2, 3),
+  tune <- list(ntrees = c(250, 500, 1000),
+               max_depth = c(1, 2),
                minobspernode = 10,
                shrinkage = 0.01)
   xgb_grid <- create.SL.xgboost(tune = tune)
@@ -322,8 +322,8 @@ generate_reduced_predictions <- function(f_hat,
   #              max_depth = c(1, 2),
   #              minobspernode = 10, shrinkage = 0.01)
   
-  tune <- list(ntrees = c(1000),
-               max_depth = c(1, 2, 3),
+  tune <- list(ntrees = c(250, 500, 1000),
+               max_depth = c(1, 2),
                minobspernode = 10,
                shrinkage = 0.01)
   xgb_grid <- create.SL.xgboost(tune = tune)
@@ -343,53 +343,6 @@ generate_reduced_predictions <- function(f_hat,
                    ncol = 1)
   
   return(list(fs_hat = fs_hat))
-}
-
-# log normal AFT regression for inclusion in survSuperLearner
-survSL.AFTreg <- function(time, event, X, newX, new.times, obsWeights, ...) {
-  
-  if(any(time == 0 & event == 1)) {
-    timepos <- as.numeric(time > 0 & event == 1)
-    fit.pos <- stats::glm(timepos ~ ., data=cbind(timepos, X)[event == 1,],
-                          family='binomial', weights = obsWeights[event == 1])
-    pos.pred <- predict(fit.pos, newdata = newX, type = 'response')
-  } else {
-    fit.pos <- 1
-    pos.pred <- rep(1, nrow(newX))
-  }
-  
-  fit.expreg <- survival::survreg(survival::Surv(time[time > 0], event[time > 0]) ~ .^2,
-                                  data = X[time > 0,],
-                                  weights = obsWeights[time > 0], dist = "lognormal")
-  pred <- predict(fit.expreg, newdata = newX, type = 'quantile', p = seq(0, .999, by=.001))
-  pred <- try(t(sapply(1:nrow(pred), function(j) {
-    pos.pred[j] * (1-stats::approx(pred[j,], seq(0, .999, by=.001),
-                                   xout = new.times, method = 'linear', rule = 2)$y)
-  })), silent = TRUE)
-  if(inherits(pred, "try-error")) stop("Survival regression failed to produce predictions.")
-  
-  fit <- list(reg.object = fit.expreg, pos.object = fit.pos)
-  class(fit) <- c("survSL.AFTreg")
-  out <- list(pred = pred, fit = fit)
-  return(out)
-}
-
-predict.survSL.AFTreg <- function(object, newX, new.times, ...) {
-  
-  if(inherits(object$pos.object, "glm")) {
-    pos.pred <- predict(object$pos.object, newdata = newX, type = 'response')
-  } else {
-    pos.pred <- rep(1, nrow(newX))
-  }
-  
-  pred <- predict(object$reg.object, newdata = newX, type = 'quantile',
-                  p = seq(0, .999, by=.001))
-  pred <- t(sapply(1:nrow(pred), function(j) {
-    pos.pred[j] * (1-stats::approx(pred[j,], seq(0, .999, by=.001),
-                                   xout = new.times, method = 'linear', rule = 2)$y)
-  }))
-  
-  return(pred)
 }
 
 CV_generate_full_predictions_landmark <- function(time,
