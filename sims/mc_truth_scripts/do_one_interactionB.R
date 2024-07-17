299eb41be3b989532e7dc5e84a880ae32476a821
Charlie Wolock <cwolock@gmail.com>
2024-07-16 23:36:51 -0400

    update mc truth variance script

--- sims/mc_truth_scripts/do_one_interactionB.R
+++ sims/mc_truth_scripts/do_one_interactionB.R
@@ -13,8 +13,14 @@ do_one <- function(n_train, correlation){
   interceptc <- 0
   beta_int <- c(0.1, -0.1, 0.1)
 
-  rho16 <- 0.7
-  rho23 <- -0.3
+  if (correlation){
+    rho16 <- 0.7
+    rho23 <- -0.3
+  } else{
+    rho16 <- 0
+    rho23 <- 0
+  }
+
 
   # benchmarks
   approx_times <- seq(0, 0.9, by = 0.01)
@@ -48,48 +54,48 @@ do_one <- function(n_train, correlation){
   }
 
   for (t in landmark_times){
-    if (scenario == "4"){
-      f <- 1-pnorm(-(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3]) -interceptt + log(t),
-                     mean = 0,
-                     sd = sdy)
-      f2 <- 1-pnorm(-(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3]) -interceptt + log(t),
-                   mean = 0,
-                   sd = sdy)
-      f_01 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
-                      mean = (beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])*rho16*x[,6],
-                      sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2*(1-rho16^2) + sdy^2))
-      f_01_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
+    # if (scenario == "4B"){
+    f <- 1-pnorm(-(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3]) -interceptt + log(t),
+                 mean = 0,
+                 sd = sdy)
+    f2 <- 1-pnorm(-(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3]) -interceptt + log(t),
+                  mean = 0,
+                  sd = sdy)
+    f_01 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
+                    mean = (beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])*rho16*x[,6],
+                    sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2*(1-rho16^2) + sdy^2))
+    f_01_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
                       mean = (beta_t[1] + beta_int[1]*x2[,2] + beta_int[3]*x2[,5])*rho16*x2[,6],
                       sd = sqrt((beta_t[1] + beta_int[1]*x2[,2] + beta_int[3]*x2[,5])^2*(1-rho16^2) + sdy^2))
-      f_02 <- 1-pnorm(-(x[,1]*beta_t[1] + beta_int[2]*x[,3]*x[,4] + beta_int[3]*x[,1]*x[,5]) - interceptt + log(t),
-                      mean = (beta_t[2] + beta_int[1]*x[,1])*rho23*x[,3],
-                      sd = sqrt((beta_t[2] + beta_int[1]*x[,1])^2*(1-rho23^2) + sdy^2))
-      f_016 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
-                       mean = 0,
-                       sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2 + sdy^2))
-      f_016_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
+    f_02 <- 1-pnorm(-(x[,1]*beta_t[1] + beta_int[2]*x[,3]*x[,4] + beta_int[3]*x[,1]*x[,5]) - interceptt + log(t),
+                    mean = (beta_t[2] + beta_int[1]*x[,1])*rho23*x[,3],
+                    sd = sqrt((beta_t[2] + beta_int[1]*x[,1])^2*(1-rho23^2) + sdy^2))
+    f_016 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
+                     mean = 0,
+                     sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2 + sdy^2))
+    f_016_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
                        mean = 0,
                        sd = sqrt((beta_t[1] + beta_int[1]*x2[,2] + beta_int[3]*x2[,5])^2 + sdy^2))
-    } else{
-      f <- 1-pnorm(-(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3]) -interceptt + log(t),
-                   mean = 0,
-                   sd = sdy)
-      f2 <- 1-pnorm(-(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3]) -interceptt + log(t),
-                    mean = 0,
-                    sd = sdy)
-      f_01 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
-                      mean = 0,
-                      sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2 + sdy^2))
-      f_01_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
-                        mean = 0,
-                        sd = sqrt((beta_t[1] + beta_int[1]*x2[,2] + beta_int[3]*x2[,5])^2 + sdy^2))
-      f_02 <- 1-pnorm(-(x[,1]*beta_t[1] + beta_int[2]*x[,3]*x[,4] + beta_int[3]*x[,1]*x[,5]) - interceptt + log(t),
-                      mean = 0,
-                      sd = sqrt((beta_t[2] + beta_int[1]*x[,1])^2* + sdy^2))
-
-      f_016 <- f_01
-      f_016_2 <- f_01_2
-    }
+    # } else{
+    #   f <- 1-pnorm(-(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3]) -interceptt + log(t),
+    #                mean = 0,
+    #                sd = sdy)
+    #   f2 <- 1-pnorm(-(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3]) -interceptt + log(t),
+    #                 mean = 0,
+    #                 sd = sdy)
+    #   f_01 <- 1-pnorm(-(x[,2]*beta_t[2] + beta_int[2]*x[,3]*x[,4]) - interceptt + log(t),
+    #                   mean = 0,
+    #                   sd = sqrt((beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])^2 + sdy^2))
+    #   f_01_2 <- 1-pnorm(-(x2[,2]*beta_t[2] + beta_int[2]*x2[,3]*x2[,4]) - interceptt + log(t),
+    #                     mean = 0,
+    #                     sd = sqrt((beta_t[1] + beta_int[1]*x2[,2] + beta_int[3]*x2[,5])^2 + sdy^2))
+    #   f_02 <- 1-pnorm(-(x[,1]*beta_t[1] + beta_int[2]*x[,3]*x[,4] + beta_int[3]*x[,1]*x[,5]) - interceptt + log(t),
+    #                   mean = 0,
+    #                   sd = sqrt((beta_t[2] + beta_int[1]*x[,1])^2* + sdy^2))
+    #
+    #   f_016 <- f_01
+    #   f_016_2 <- f_01_2
+    # }
 
     # brier
     V_0 <- survML:::estimate_brier(time = dat$y,
@@ -114,26 +120,26 @@ do_one <- function(n_train, correlation){
                                     S_hat = S0,
                                     G_hat = G0)
     V_016 <- survML:::estimate_brier(time = dat$y,
-                                    event = dat$delta,
-                                    approx_times = approx_times,
-                                    tau = t,
-                                    preds = f_016,
-                                    S_hat = S0,
-                                    G_hat = G0)
+                                     event = dat$delta,
+                                     approx_times = approx_times,
+                                     tau = t,
+                                     preds = f_016,
+                                     S_hat = S0,
+                                     G_hat = G0)
     V_016_2 <- survML:::estimate_brier(time = dat2$y,
+                                       event = dat2$delta,
+                                       approx_times = approx_times,
+                                       tau = t,
+                                       preds = f_016_2,
+                                       S_hat = S02,
+                                       G_hat = G02)
+    V_0_2 <- survML:::estimate_brier(time = dat2$y,
                                      event = dat2$delta,
                                      approx_times = approx_times,
                                      tau = t,
-                                     preds = f_016_2,
+                                     preds = f2,
                                      S_hat = S02,
                                      G_hat = G02)
-    V_0_2 <- survML:::estimate_brier(time = dat2$y,
-                                   event = dat2$delta,
-                                   approx_times = approx_times,
-                                   tau = t,
-                                   preds = f2,
-                                   S_hat = S02,
-                                   G_hat = G02)
 
     var_01 <- mean((V_0$EIF - V_01$EIF)^2)
     var_02 <- mean((V_0$EIF - V_02$EIF)^2)
@@ -157,13 +163,13 @@ do_one <- function(n_train, correlation){
                                  G_hat = G0,
                                  robust = TRUE)
     V_0_2 <- survML:::estimate_AUC(time = dat2$y,
-                                 event = dat2$delta,
-                                 approx_times = approx_times,
-                                 tau = t,
-                                 preds = 1-f2,
-                                 S_hat = S02,
-                                 G_hat = G02,
-                                 robust = TRUE)
+                                   event = dat2$delta,
+                                   approx_times = approx_times,
+                                   tau = t,
+                                   preds = 1-f2,
+                                   S_hat = S02,
+                                   G_hat = G02,
+                                   robust = TRUE)
     V_01 <- survML:::estimate_AUC(time = dat$y,
                                   event = dat$delta,
                                   approx_times = approx_times,
@@ -181,21 +187,21 @@ do_one <- function(n_train, correlation){
                                   G_hat = G0,
                                   robust = TRUE)
     V_016 <- survML:::estimate_AUC(time = dat$y,
-                                  event = dat$delta,
-                                  approx_times = approx_times,
-                                  tau = t,
-                                  preds = 1-f_016,
-                                  S_hat = S0,
-                                  G_hat = G0,
-                                  robust = TRUE)
-    V_016_2 <- survML:::estimate_AUC(time = dat2$y,
-                                   event = dat2$delta,
+                                   event = dat$delta,
                                    approx_times = approx_times,
                                    tau = t,
-                                   preds = 1-f_016_2,
-                                   S_hat = S02,
-                                   G_hat = G02,
+                                   preds = 1-f_016,
+                                   S_hat = S0,
+                                   G_hat = G0,
                                    robust = TRUE)
+    V_016_2 <- survML:::estimate_AUC(time = dat2$y,
+                                     event = dat2$delta,
+                                     approx_times = approx_times,
+                                     tau = t,
+                                     preds = 1-f_016_2,
+                                     S_hat = S02,
+                                     G_hat = G02,
+                                     robust = TRUE)
 
     var_01 <- mean((V_0$EIF - V_01$EIF)^2)
     var_02 <- mean((V_0$EIF - V_02$EIF)^2)
@@ -232,22 +238,22 @@ do_one <- function(n_train, correlation){
   }
 
   for (t in c_landmark_times){
-    if (scenario == "4B"){
-      f <- -(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3])
-      f2 <- -(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3])
-      f_01 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4] + (beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])*rho16*x[,6])
-      f_02 <- -(x[, 1] * beta_t[1] + beta_int[2]*x2[,3]*x2[,4] + (beta_t[2] + beta_int[1]*x[,1])*rho23*x[,3])
-      f_016 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4])
-      f_016_2 <- -(x2[, 2] * beta_t[2]+ beta_int[2]*x2[,3]*x2[,4])
-    } else{
-      f <- -(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3])
-      f2 <- -(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3])
-      f_01 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4])
-      f_01_2 <- -(x2[, 2] * beta_t[2] + beta_int[2]*x2[,3]*x2[,4])
-      f_02 <- -(x[, 1] * beta_t[1]+ beta_int[2]*x[,3]*x[,4])
-      f_016 <- f_01
-      f_016_2 <- f_01_2
-    }
+    # if (scenario == "4B"){
+    f <- -(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3])
+    f2 <- -(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3])
+    f_01 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4] + (beta_t[1] + beta_int[1]*x[,2] + beta_int[3]*x[,5])*rho16*x[,6])
+    f_02 <- -(x[, 1] * beta_t[1] + beta_int[2]*x2[,3]*x2[,4] + (beta_t[2] + beta_int[1]*x[,1])*rho23*x[,3])
+    f_016 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4])
+    f_016_2 <- -(x2[, 2] * beta_t[2]+ beta_int[2]*x2[,3]*x2[,4])
+    # } else{
+    #   f <- -(x %*% beta_t + x[,1]*x[,2]*beta_int[1] + x[,3]*x[,4]*beta_int[2] + x[,1]*x[,5]*beta_int[3])
+    #   f2 <- -(x2 %*% beta_t + x2[,1]*x2[,2]*beta_int[1] + x2[,3]*x2[,4]*beta_int[2] + x2[,1]*x2[,5]*beta_int[3])
+    #   f_01 <- -(x[, 2] * beta_t[2] + beta_int[2]*x[,3]*x[,4])
+    #   f_01_2 <- -(x2[, 2] * beta_t[2] + beta_int[2]*x2[,3]*x2[,4])
+    #   f_02 <- -(x[, 1] * beta_t[1]+ beta_int[2]*x[,3]*x[,4])
+    #   f_016 <- f_01
+    #   f_016_2 <- f_01_2
+    # }
 
     V_0 <- survML:::estimate_cindex(time = dat$y,
                                     event = dat$delta,
