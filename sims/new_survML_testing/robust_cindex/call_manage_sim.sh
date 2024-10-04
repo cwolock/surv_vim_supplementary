0d6a473137ae2977a5ee9c84a5627e00d318c893
Charlie Wolock <cwolock@gmail.com>
2024-10-03 22:39:48 -0400

    add robustness sim for cindex

---
+++ sims/new_survML_testing/robust_cindex/call_manage_sim.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+Rscript /home/cwolock/surv_vim_supplementary/sims/new_survML_testing/robust_cindex/manage_sim.R
---
+++ sims/new_survML_testing/robust_cindex/load_results.R
@@ -0,0 +1,25 @@
+#!/usr/local/bin/Rscript
+
+sim_name <- "new_survML_testing_robust_cindex"
+nreps_total <- 500
+nreps_per_job <- 1
+
+output_dir <- "output/"
+
+n_trains <- c(500, 750, 1000, 1250, 1500)
+misspec_types <- c("none", "censoring", "event_plusf0")
+
+njobs_per_combo <- nreps_total/nreps_per_job
+
+param_grid <- expand.grid(mc_id = 1:njobs_per_combo,
+                          n_train = n_trains,
+                          misspec_type = misspec_types)
+
+output_nms <- paste0(sim_name, "_", 1:dim(param_grid)[1], ".rds")
+avail_nms <- list.files(output_dir, pattern = paste0(sim_name, "_*"))
+names_to_try <- output_nms[which(output_nms %in% avail_nms)]
+missing <- output_nms[which(!(output_nms %in% avail_nms))]
+print(missing)
+output_lst <- lapply(paste0(output_dir, names_to_try), readRDS)
+output_df <- do.call(rbind.data.frame, output_lst)
+saveRDS(output_df, paste0(sim_name, ".rds"))
